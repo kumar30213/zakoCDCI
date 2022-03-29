0d3145cfb18d3f3d72b72ec9a31cfdbd740bcb5f
Rakeshkumar Yadav <kumar30213@gmail.com>
2022-03-29 19:09:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    a = 5
+    b = 6
+    c = a+b
+    print(c)
+    print("go to hell")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Rakeshkumar Yadav')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
